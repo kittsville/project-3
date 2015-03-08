84575b746805748f2bffb2785cae7783544d273b
ptravers <traverspw@gmail.com>
2015-03-08 19:51:48 +0000

    fixed module adding

--- sudoku.pl
+++ sudoku.pl
@@ -3,9 +3,9 @@
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_header)).
 :- use_module(library(http/http_client)).
-:- use_module(library(http/http_json.pl)).
-:- use_module(library(json.pl)).
-:- use_module(library(json_convert.pl)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/json.pl)).
+:- use_module(library(http/json_convert)).
 
 sudoku(Rows) :- 
   append(Rows, Vs), Vs ins 1..9,
@@ -38,10 +38,12 @@ server(Port) :-
 :- http_handler('/sudoku.css', http_reply_file('sudoku.css', []), []).
 :- http_handler('/sudoku.js', http_reply_file('sudoku.js', []),[]).
 :- http_handler('/puzzles.js', http_reply_file('puzzles.js', []),[]).
-:- http_post('/', json(Request), handle(Request), []).
-handle(Request) :-
+:- http_post('/[object20%Object]', json(Request), reply, []).
+reply(Request) :-
     http_read_json(Request, JSONIn),
     json_to_prolog(JSONIn, PrologIn),
     sudoku(PrologIn, PrologOut),
     prolog_to_json(PrologOut, JSONOut),
     reply_json(JSONOut).
+
+
